58595728a5da6a9005a6cb2332f681200be3d22f
lasenkoa <lasenkoa@gmail.com>
2024-05-10 19:41:03 +0200

    breeze init

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,15 @@
 <?php
 
-use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
-Route::get('register', [RegisterController::class, 'create']);
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
-Route::get('/login', function () {
-    return view('login'); 
-});
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
