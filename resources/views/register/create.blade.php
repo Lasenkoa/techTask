44d5c8bf242399c422568e3f37643f13aa27db31
lasenkoa <lasenkoa@gmail.com>
2024-05-10 14:17:27 +0200

    register init

---
+++ resources/views/register/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('content')
+        <section class="px-6 py-8">
+            <!-- <main class="max-w-lg mx-auto">
+                <form method="POST" action="/">
+
+                </form>
+            </main> -->
+            <h1>Hello World</h1>
+        </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,13 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/register', function () {
-    return view('register');
-});
+Route::get('register', [RegisterController::class, 'create']);
 
 Route::get('/login', function () {
     return view('login'); 
