7bd8594f40a89814c405eacb463dfbf6330f5f4f
lasenkoa <lasenkoa@gmail.com>
2024-05-11 14:22:16 +0200

    usertable init

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,6 +11,9 @@
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
                 </div>
+                <x-usertable>
+
+                </x-usertable>
             </div>
         </div>
     </div>
